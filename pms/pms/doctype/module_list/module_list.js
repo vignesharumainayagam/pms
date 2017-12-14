b64016a691471e31c23d7ee04fcc54450417f5c1
Administrator <vigneshwaran@valiantsystems.com>
2017-12-14 17:18:40 +0530

    asd

--- pms/pms/doctype/module_list/module_list.js
+++ pms/pms/doctype/module_list/module_list.js
@@ -10,50 +10,37 @@ frappe.ui.form.on('Module List', {
 });
 
 
-// frappe.ui.form.on("Module List", "onload", function(frm){
-// frappe.call({
-// method: "pms.pms.doctype.module_list.module_list.get_screens",
-// args: {
-//             project: frm.doc.project,
-//             module: frm.doc.name
-//                     },
-// callback: function (r) {
-// 			data = r.message;
-// 			for (var k=0; k < data.length; k++){
-// 			frappe.model.add_child(cur_frm.doc, "Module Screen Child", "table_9"); 
-// 			$.each(frm.doc.table_9 || [], function(e, v){
-// 			frappe.model.set_value(v.doctype, v.name, "screen_name", data[k][0])
-// 			})
+frappe.ui.form.on("Module List", "onload", function(frm){
 
-// 			}
+if(!frm.doc.__islocal)
+{
+cur_frm.save()
+frappe.call({
+method: "pms.pms.doctype.module_list.module_list.get_screens",
+args: {
+            project: frm.doc.project,
+            module: frm.doc.name
+                    },
+callback: function (r) {
+console.log(r.message)
+
+for(var e=0; e<r.message.length; e++)
+    {
+        frappe.model.add_child(cur_frm.doc, "Module Screen Child", "table_9");  
+        $.each(frm.doc.table_9 || [], function(e, v) {
+        frappe.model.set_value(v.doctype, v.name, "screen_name", r.message[e][0])
+        })
+        frm.refresh_field("table_9");
+    }
+
+		}
 
 
-// 		}
+	})
+}
+else {
+	return 0;
+}
+});
 
-// 	})
 
-// });
-
-
-
-
-// frappe.ui.form.on("Module List", "onload", function(frm){
-// frappe.call({
-// method: "pms.pms.doctype.module_list.module_list.delete",
-// args: {
-//             project: frm.doc.project,
-//             module: frm.doc.name
-//                     },
-// callback: function (r) {
-// cur_frm.save();
-// }
-// })
-// });
-
-// for(var i=0; i<r.message.length; i++){
-// frappe.model.add_child(cur_frm.doc, "Module Screen Child", "table_9");  	
-// $.each(frm.doc.table_9 || [], function(e, v) {
-// frappe.model.set_value(v.doctype, v.name, "screen_name", r.message[i][0])
-// })
-//  frm.refresh_field("table_9");
-// }

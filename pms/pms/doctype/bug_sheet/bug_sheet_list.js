902aaa11602d13e95c578c9e2cf06a99f07fc8b9
Tridots <vigneshwaran@valiantsystems.com>
2017-12-15 17:05:51 +0530

    wqe

--- pms/pms/doctype/bug_sheet/bug_sheet_list.js
+++ pms/pms/doctype/bug_sheet/bug_sheet_list.js
@@ -1,5 +1,5 @@
 frappe.listview_settings['Bug Sheet'] = {
-	add_fields: ["status"],
+	add_fields: ["status", "priority"],
 	get_indicator: function(doc) {
 		if(doc.status=="Fixed") {
 			return [__("Fixed"), "orange"];
@@ -9,6 +9,19 @@ frappe.listview_settings['Bug Sheet'] = {
 			return [__("Open"), "red"];
 		} else if(doc.status=="Re-open") {
 			return [__("Re-open"), "red"];
+		} else if(doc.status=="Closed") {
+			return [__("Closed"), "blue"];
 		}
+		if(doc.priority=="High") {
+			return [__("High"), "red"];
+		} else if(doc.priority=="Medium") {
+			return [__("Medium"), "black"];
+		} else if(doc.priority=="Low") {
+			return [__("Low"), "green"];
+		}
+
 	}
+
 };
+
+
